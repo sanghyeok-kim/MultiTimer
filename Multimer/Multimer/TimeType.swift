5c11839ce7108e3e4865b0861e2aef427ec3c685
sanghyeok-kim <slsnsi1212@gmail.com>
2022-11-03 14:50:35 +0900

    #3 feat: TimeType 열거형 구현

---
+++ Multimer/Multimer/TimeType.swift
@@ -0,0 +1,36 @@
+//
+//  TimeType.swift
+//  Multimer
+//
+//  Created by 김상혁 on 2022/11/03.
+//
+
+import Foundation
+
+enum TimeType: CaseIterable {
+    case hour
+    case minute
+    case second
+    
+    var title: String {
+        switch self {
+        case .hour:
+            return "시간"
+        case .minute:
+            return "분"
+        case .second:
+            return "초"
+        }
+    }
+    
+    var range: [Int] {
+        switch self {
+        case .hour:
+            return (0...999).map { $0 }
+        case .minute:
+            return (0...59).map { $0 }
+        case .second:
+            return (0...59).map { $0 }
+        }
+    }
+}
