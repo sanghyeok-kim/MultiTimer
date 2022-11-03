ca06ab322bfac144fd96f56615b2e05397c0b26c
sanghyeok-kim <slsnsi1212@gmail.com>
2022-11-03 14:52:02 +0900

    #3 feat: Timer 구조체 구현
    TODO
    - time의 자료형을 String이 아닌 별도의 자료형으로 변경 필요
    - sound, repeat 속성에 대한 설정 필요

---
+++ Multimer/Multimer/Timer.swift
@@ -0,0 +1,17 @@
+//
+//  Timer.swift
+//  Multimer
+//
+//  Created by 김상혁 on 2022/11/03.
+//
+
+import Foundation
+
+struct Timer {
+    let id: UUID
+    var name: String
+    var tag: String
+    var time: String // FIXME: String -> Int나 Time 객체 등으로 변경
+//    var sound:
+//    var repeatCount
+}
