fcdcbe8dcc34668c46356a3e87a65f0618e2e6ff
sanghyeok-kim <slsnsi1212@gmail.com>
2022-11-05 23:02:25 +0900

    feat: Tag 모델 구현

---
+++ Multimer/Multimer/Tag.swift
@@ -0,0 +1,14 @@
+//
+//  Tag.swift
+//  Multimer
+//
+//  Created by 김상혁 on 2022/11/03.
+//
+
+import Foundation
+
+struct Tag {
+    let id: UUID
+    var name: String
+    var icon: Int // FIXME: Int -> 색상 또는 이모지로 변경
+}
