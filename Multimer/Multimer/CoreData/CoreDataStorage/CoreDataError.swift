90a52a74ef9b0b2944454609ffe23e0d264a210c
sanghyeok-kim <slsnsi1212@gmail.com>
2022-11-29 23:13:29 +0900

    #12 feat: CoreDataError 구현

---
+++ Multimer/Multimer/CoreData/CoreDataStorage/CoreDataError.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataError.swift
+//  Multimer
+//
+//  Created by 김상혁 on 2022/11/29.
+//
+
+import Foundation
+
+enum CoreDataError: Error {
+    case cannotFetch
+    case cannotFindTarget
+}
