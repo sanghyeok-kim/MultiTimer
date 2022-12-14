21e1ba91b082794592a55c99d7278ecdce4accd0
sanghyeok-kim <slsnsi1212@gmail.com>
2022-12-14 19:03:54 +0900

    #16 remove: 불필요한 주석 코드 삭제

--- Multimer/Multimer/CoreData/CoreDataStorage/CoreDataStorage+TimerMO.swift
+++ Multimer/Multimer/CoreData/CoreDataStorage/CoreDataStorage+TimerMO.swift
@@ -9,26 +9,7 @@ import Foundation
 import CoreData
 
 extension CoreDataStorage {
-//    func update(
-//        timerMO: TimerMO,
-//        identifier: UUID? = nil,
-//        name: String? = nil,
-//        tag: Tag? = nil,
-//        time: Time? = nil
-//    ) {
-//        backgroundContext.perform { [weak self] in
-//            guard let self = self else { return }
-//            timerMO.update(
-//                name: name,
-//                tag: tag,
-//                time: time,
-//                context: self.backgroundContext
-//            )
-//            self.saveContext()
-//        }
-//    }
     
-    // TODO: 삭제
     func update(
         timerMO: TimerMO,
         identifier: UUID? = nil,
