39c080050c3ac8917ac4ff10e0064ce69f2ae181
sanghyeok-kim <slsnsi1212@gmail.com>
2022-12-30 23:54:59 +0900

    refactor: TagColor를 CustomColor로 변경

--- Multimer/Multimer/TagColor.swift
+++ Multimer/Multimer/TagColor.swift
@@ -9,29 +9,33 @@ import UIKit
 import CoreData
 
 enum TagColor: Int, CaseIterable, Codable {
+    case label
     case red
-    case blue
-    case green
-    case purple
-    case yellow
-    case brown
-    case magenta
-    case pink
     case orange
-    case indigo
+    case pink
+    case green
+    case cyan
+    case purple
+    case navy
     
-    var rgb: UIColor {
+    var uiColor: UIColor {
         switch self {
-        case .red: return UIColor.red
-        case .blue: return UIColor.blue
-        case .green: return UIColor.green
-        case .purple: return UIColor.purple
-        case .yellow: return UIColor.yellow
-        case .brown: return UIColor.brown
-        case .magenta: return UIColor.magenta
-        case .pink: return UIColor.systemPink
-        case .orange: return UIColor.orange
-        case .indigo: return UIColor.systemIndigo
+        case .label:
+            return CustomColor.Tag.label
+        case .red:
+            return CustomColor.Tag.red
+        case .orange:
+            return CustomColor.Tag.orange
+        case .pink:
+            return CustomColor.Tag.pink
+        case .green:
+            return CustomColor.Tag.green
+        case .cyan:
+            return CustomColor.Tag.cyan
+        case .purple:
+            return CustomColor.Tag.purple
+        case .navy:
+            return CustomColor.Tag.navy
         }
     }
 }
