d308dfaa9ca97121083b9da8fd682dab0cd93807
sanghyeok-kim <slsnsi1212@gmail.com>
2023-01-02 00:21:10 +0900

    refactor: UIPickerView+setFixedLabels 메소드 오프셋 조정

--- Multimer/Multimer/Extension/UIPickerView+setFixedLabels.swift
+++ Multimer/Multimer/Extension/UIPickerView+setFixedLabels.swift
@@ -1,5 +1,5 @@
 //
-//  UIPickerView+SetFixedLabels.swift
+//  UIPickerView+setFixedLabels.swift
 //  Multimer
 //
 //  Created by 김상혁 on 2022/11/03.
@@ -11,7 +11,7 @@ extension UIPickerView {
     func setFixedLabels(with texts: [String]) {
         let columCount = texts.count
         let fontSize = UIFont.labelFontSize
-        
+
         let labels = (0..<columCount).map { index -> UILabel in
             let label = UILabel()
             label.text = texts[index]
@@ -20,11 +20,12 @@ extension UIPickerView {
             return label
         }
         
-        let pickerWidth: CGFloat = self.frame.width - fontSize
+        let pickerWidth: CGFloat = UIScreen.main.bounds.size.width
         let labelY: CGFloat = (self.frame.size.height / 2) - (fontSize / 2)
-        
+
         for (index, label) in labels.enumerated() {
-            let labelX: CGFloat = (pickerWidth / CGFloat(columCount)) * CGFloat(index + 1) - fontSize
+            let offset = fontSize - CGFloat(index) * CGFloat(columCount + 1)
+            let labelX: CGFloat = (pickerWidth / CGFloat(columCount + 1)) * CGFloat(index + 1) - offset
             label.frame.origin = CGPoint(x: labelX, y: labelY)
             self.addSubview(label)
         }
