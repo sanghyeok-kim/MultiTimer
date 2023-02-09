853a1e33da5571e5b90b3a58d27718a727bd6557
sanghyeok-kim <slsnsi1212@gmail.com>
2023-02-09 17:25:13 +0900

    #24 feat: extension으로 showSwipeToStopNotice에 대한 NotificationName 추가

---
+++ Multimer/Multimer/Presentation/Support/Extension/NotificationName+CustomName.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName+CustomName.swift
+//  Multimer
+//
+//  Created by 김상혁 on 2023/02/09.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let showSwipeToStopNotice = Notification.Name(Constant.NotificationCenter.showSwipeToStopNotice)
+}
