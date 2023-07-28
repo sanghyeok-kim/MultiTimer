4233d68712710111c6be5d7be03eef1a621119d4
sanghyeok-kim <slsnsi1212@gmail.com>
2023-07-28 22:23:28 +0900

    #38 refactor: SceneDelegate에서 HomeCoordinator를 통해 화면 플로우를 시작하도록 변경

--- Multimer/Multimer/Application/SceneDelegate.swift
+++ Multimer/Multimer/Application/SceneDelegate.swift
@@ -8,18 +8,19 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
     var window: UIWindow?
+    private var homeCoordinator: HomeCoordinator?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let initialViewController = MainViewController()
-        let navVC = UINavigationController(rootViewController: initialViewController)
-        let viewModel = MainViewModel(
-            mainUseCase: DefaultMainUseCase(timerPersistentRepository: CoreDataTimerRepository())
-        )
-        initialViewController.bind(viewModel: viewModel)
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = navVC
+        
+        let rootNavigationController = UINavigationController()
+        homeCoordinator = DefaultHomeCoordinator(navigationController: rootNavigationController)
+        homeCoordinator?.start()
+        
+        window?.rootViewController = rootNavigationController
         window?.makeKeyAndVisible()
     }
 }
