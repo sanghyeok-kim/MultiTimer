3ec697d247af4d1e7dbb8ec64a7c6d9332fbd208
sanghyeok-kim <slsnsi1212@gmail.com>
2023-08-04 08:05:02 +0900

    #8 feat: RingtoneCellModel 구현

---
+++ Multimer/Multimer/Presentation/RingtoneSelect/RingtoneCellModel.swift
@@ -0,0 +1,13 @@
+//
+//  RingtoneCellModel.swift
+//  Multimer
+//
+//  Created by 김상혁 on 2023/08/02.
+//
+
+import Foundation
+
+struct RingtoneCellModel: Hashable {
+    var isSelected: Bool = false
+    var ringtone: Ringtone
+}
